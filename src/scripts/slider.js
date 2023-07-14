c728cc13e79cbf7410ee9c3ab3b83fa1cfe0c038
EverOspino <everospino97@gmail.com>
2023-07-14 03:06:37 -0500

    proyecto casi terminado

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', (e) => {
-    let slider = document.getElementsByClassName('slider')[0];
+    let slider = document.getElementById('slider');
     let scrollLeftMax = slider.clientWidth * (slider.children.length - 1);
 
-    let btnAfter = document.getElementsByClassName('btn-after')[0];
-    let btnBefore = document.getElementsByClassName('btn-before')[0];
+    let btnAfter = document.getElementById('btn-after');
+    let btnBefore = document.getElementById('btn-before');
 
     const changeEndpointSlider = (endpoint) => {
         slider.classList.remove('scroll_behavior');
@@ -11,17 +11,19 @@ window.addEventListener('load', (e) => {
         slider.classList.add('scroll_behavior');
     }
     
-    btnAfter.addEventListener('click', (e) => {
-        if (slider.scrollLeft === scrollLeftMax) {
+    btnAfter.addEventListener('click', () => {
+        if (slider.scrollLeft - scrollLeftMax >= 50 || slider.scrollLeft - scrollLeftMax >= -50) {
             changeEndpointSlider(0);
         }
-        slider.scrollLeft = slider.scrollLeft + slider.clientWidth;
+        // slider.scrollLeft = slider.scrollLeft + slider.clientWidth;
+        slider.scrollBy(slider.clientWidth, 0);
     });
 
-    btnBefore.addEventListener('click', (e) => {
+    btnBefore.addEventListener('click', () => {
         if (slider.scrollLeft === 0) {
             changeEndpointSlider(scrollLeftMax);
         }
-        slider.scrollLeft = slider.scrollLeft - slider.clientWidth;
+        // slider.scrollLeft = slider.scrollLeft - slider.clientWidth;
+        slider.scrollBy(-slider.clientWidth, 0);
     });
 })
